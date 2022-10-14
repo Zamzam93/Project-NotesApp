be218a33d23c77250da560fad2ab3176daf76006
Zamzam93 <samira0101@live.com>
2022-10-14 13:14:02 -0400

    working on completing part4

---
+++ src/main/resources/static/JavaScript/register.js
@@ -0,0 +1,3 @@
+const registerForm = document.getElementById('register-form')
+const registerUsername = document.getElementById('register-username')
+const registerPassword = document.getElementById('register-password')
